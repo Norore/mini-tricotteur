ba5ce683c595e8a8f383117054e942f9364c866e
Nolwenn Lavielle <sky_nolwenn@yahoo.fr>
2012-12-09 08:17:16 -0800

    Create apropos.py

---
+++ apropos.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import gtk
+
+class Apropos:
+
+    def __init__(self):
+        self.about = gtk.AboutDialog()
+        self.about.set_program_name("Mon programme tricot")
+        self.about.set_version("0.1")
+        self.about.set_copyright("CC-by-SA Nolwenn Lavielle")
+        self.about.set_comments('''Mon programme tricot est un programme simple vous permettant de calculer rapidement les différentes longueurs pour vos mesures de tricot.''')
+        self.about.run()
+        self.about.destroy()
